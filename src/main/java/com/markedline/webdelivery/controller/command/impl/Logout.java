7bbdc1a19215ca60803a0534e5f94cd113fc0fcc
doppelmarker <doppelmarker@gmail.com>
2021-03-11 17:57:14 +0300

    change session behaviour after logout: remove 'auth' attr -> invalidate session

--- src/main/java/com/markedline/webdelivery/controller/command/impl/Logout.java
+++ src/main/java/com/markedline/webdelivery/controller/command/impl/Logout.java
@@ -5,17 +5,16 @@ import com.markedline.webdelivery.controller.command.Command;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class Logout implements Command {
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
+//        if (session != null) {
+//            session.removeAttribute("auth");
+//        }
 
-        if (session != null) {
-            session.removeAttribute("auth");
-        }
+        request.getSession().invalidate();
 
         response.sendRedirect("Controller?command=gotoindexpage&message=logout ok");
     }
